239ce56b73ff4d7656e3dab223d98b717f0452ca
Eusebiu Popescu <popescusebi1234@yahoo.com>
2025-01-14 20:43:02 +0200

    add more changes

--- src/main/java/com/sebi/service/CartItemServiceImplementation.java
+++ src/main/java/com/sebi/service/CartItemServiceImplementation.java
@@ -27,7 +27,7 @@ public class CartItemServiceImplementation implements CartItemService{
     }
     @Override
     public CartItem createCartItem(CartItem cartItem) {
-//        cartItem.setQuantity(1);//
+//        cartItem.setQuantity(1);////
         cartItem.setPrice(cartItem.getProduct().getPrice()*cartItem.getQuantity());
         cartItem.setDiscountedPrice(cartItem.getProduct().getDiscountedPrice()*cartItem.getQuantity());
 
