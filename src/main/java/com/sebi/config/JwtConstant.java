19e8ba45705115f9fb45d53ee52a3aa35b83254a
Eusebiu Popescu <popescusebi1234@yahoo.com>
2025-01-22 09:45:24 +0200

    add more changes

--- src/main/java/com/sebi/config/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package com.sebi.config;
-
-public class JwtConstant {
-
-    public static final String SECRET_KEY="bskasskdkkasakdkajadkjdjdajajsdj jdjdjjsjjsjdjsjsjdj";
-    public static final String JWT_HEADER="Authorization";
-}
